8e4f8c61f8532415639bb858a87eb1c8552b2647
Mariana Siqueira <furrielmar@gmail.com>
2021-10-17 23:32:45 -0300

    Border-radius Previewer terminado

--- projetos/Border-radius Previewer/brp.js
+++ projetos/Border-radius Previewer/brp.js
@@ -1,68 +1,46 @@
-function calculaBorda(){
-    var pixel = "px"
-    
+function borda(){
 
-    if ((document.getElementById("topLeft").value)!="") {
-        document.getElementById("container").style.borderRadius = document.getElementById("topLeft").value+pixel
-        exibir("topLeft")
-    }    
+    let topLeft = document.getElementById("topLeft").value;
+    let topRight = document.getElementById("topRight").value;
+    let botRight = document.getElementById("botRight").value;
+    let botLeft = document.getElementById("botLeft").value;
+    let container = document.getElementById("container").style;
+    let textTL = document.getElementById("topL");
+    let textTR = document.getElementById("topR");
+    let textBL = document.getElementById("botL");
+    let textBR = document.getElementById("botR");
 
-    if ((document.getElementById("topRight").value)!="") {
-        document.getElementById("container").style.borderTopRightRadius = document.getElementById("topRight").value+pixel
-        exibir("topRight")
+    if (topLeft!=""){
+        container.borderRadius = topLeft+"px";
+        textTL.innerHTML = "border-top-left-radius: "+container.borderRadius+"; <br>";
+    }
+
+    if (topRight!=""){
+        container.borderTopRightRadius = topRight + "px";        
+        textTR.innerHTML = "border-top-right-radius: "+container.borderTopRightRadius+"<br>";  
+        textTR.style.display = "block";
     }
     
-    
-    if ((document.getElementById("botLeft").value)!="") {
-        document.getElementById("container").style.borderBottomLeftRadius = document.getElementById("botLeft").value+pixel
-        exibir("botLeft")
-    } 
+    if (botRight!=""){
+        container.borderBottomRightRadius = botRight + "px";
+        textBR.innerHTML = "border-bot-right-radius: "+container.borderBottomRightRadius+"<br>"  ;
+        textBR.style.display = "block";
 
-    if ((document.getElementById("botRight").value)!="") {
-        document.getElementById("container").style.borderBottomRightRadius = document.getElementById("botRight").value+pixel
-        exibir("botRight")
     }
-    if (((document.getElementById("topLeft").value)!="")&&
-    ((document.getElementById("topRight").value)=="0")&&
-    ((document.getElementById("botLeft").value)=="0")&&
-    (((document.getElementById("botRight").value)=="0"))) {
-        exibir("somenteTopLeft")
-    }  
-}
 
-function limpar(){
-    document.getElementById("container").style.borderRadius = "0px"
-    document.getElementById("topL").style.display = "none"
-    document.getElementById("topR").style.display = "none"
-    document.getElementById("botL").style.display = "none"
-    document.getElementById("botR").style.display = "none"
-}
-
-function exibir(a){
-    switch (a){
-        case "topLeft":
-            document.getElementById("topL").innerHTML = "border-radius:  "+document.getElementById("topLeft").value + "px;"
-            document.getElementById("topL").style.display = "block"
-            break
-        case "topRight":
-            document.getElementById("topR").innerHTML = "border-top-right-radius: "+document.getElementById("topRight").value + "px;"
-            document.getElementById("topR").style.display = "block"
-            break
-        case "botLeft":
-            document.getElementById("botL").innerHTML = "border-bottom-left-radius: "+document.getElementById("botLeft").value + "px;"
-            document.getElementById("botL").style.display = "block"
-            break
-        case "botRight":
-            document.getElementById("botR").innerHTML = "border-bottom-left-radius: "+document.getElementById("botRight").value + "px;"
-            document.getElementById("botR").style.display = "block"
-            break
-        case "somenteTopLeft":
-            document.getElementById("topL").innerHTML = "border-top-left-radius: "+document.getElementById("topLeft").value + "px;"
-            document.getElementById("topL").style.display = "block"
-            document.getElementById("topR").style.display = "none"
-            document.getElementById("botL").style.display = "none"
-            document.getElementById("botR").style.display = "none"
+    if (botLeft!=""){
+       container.borderBottomLeftRadius = botLeft + "px";
+       textBL.innerHTML = "border-bot-left-radius: "+container.borderBottomLeftRadius+"<br>"  ;
+       textBL.style.display = "block";
     }
-    
 
+    if (((topLeft!="")&&(topRight=="")&&(botLeft=="")&&(botRight=="")) || ((topLeft == topRight)&&(topRight == botRight)&&(botRight==botLeft))){    
+        container.borderRadius = topLeft+"px";
+        textTL.innerHTML = "border-radius: "+container.borderRadius+"<br>"
+        //textTL.style.display = "none;"
+        textBL.style.display = "none";
+        textTR.style.display = "none";
+        textBR.style.display = "none";
+
+    }
 }
